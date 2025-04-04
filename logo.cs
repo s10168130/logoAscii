70037f571eadf96fa4db6f0235b74b57f0508e62
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-04-04 07:46:58 +0000

    Add project files.

---
+++ logo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+
+using System.Drawing;
+namespace logoAscii
+{
+    public class logo
+    {
+        public logo()
+        {//constructor
+
+            //get full location of the project
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+            //replace the debug and bin   bin/Debug
+            string new_location = full_location.Replace("bin\\Debug\\","");
+            //COMBINE PATH
+            string full_path = Path.Combine(new_location, "LOGO.jpg");
+            //time to use ascii
+            //creating the bitmap class
+            Bitmap image = new Bitmap(full_path);
+            //then set size
+            image = new Bitmap(image,new Size(210,200) );
+
+
+
+            //outer inner loop
+            for (int height = 0; height < image.Height; height++)
+            {
+                
+                for (int width = 0; width < image.Width; width++)
+                {                                    
+                    Color pixelColor = image.GetPixel(width,height );
+                    int gray = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+                    char assciChar = gray > 200 ? '.' : gray > 150 ? '*' : gray > 100 ? 'O' : gray > 50 ? '#' : '@';
+                    Console.Write(assciChar);
+                }
+                Console.WriteLine();            }
+        }//end of costructor        
+
+    }//end of class
+
+}//end of namespace
